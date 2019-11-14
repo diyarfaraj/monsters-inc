2f1d5b5048918657d2fe740c48350da0ec7803bc
Diyar Faraj <diyar.faraj@gmail.com>
2019-11-14 06:58:40 +0100

    Updates

---
+++ precache-manifest.7f9b0f263628e3756addf62b21512834.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57d44b875cd9e39d4b5494daad2cd7f",
+    "url": "/monsters-inc/index.html"
+  },
+  {
+    "revision": "99b5b1799d54f2a87333",
+    "url": "/monsters-inc/static/css/main.20f1abfb.chunk.css"
+  },
+  {
+    "revision": "2e71d30d35e433b31290",
+    "url": "/monsters-inc/static/js/2.a08650c1.chunk.js"
+  },
+  {
+    "revision": "99b5b1799d54f2a87333",
+    "url": "/monsters-inc/static/js/main.3cd27425.chunk.js"
+  },
+  {
+    "revision": "7ac0b21287e33a643194",
+    "url": "/monsters-inc/static/js/runtime-main.2959327d.js"
+  }
+]);
